6c91d7fff905be22b83f14b7706f29e8eb2c8dff
Yogesh Singh <yogeshsingh3596@gmail.com>
2023-02-12 17:52:13 +0530

    Pizza

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -26,9 +26,41 @@ public class Pizza {
         bill="Base Price Of The Pizza: "+this.price+"\n";
     }
 
-    public int getPrice(){
-        return this.price;
+    // getters & setters
+
+    public int getPrice() {
+        return price;
     }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Boolean getVeg() {
+        return isVeg;
+    }
+
+    public void setVeg(Boolean veg) {
+        isVeg = veg;
+    }
+
+    public void setBill(String bill) {
+        this.bill = bill;
+    }
+
+    public int getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(int toppings) {
+        this.toppings = toppings;
+    }
+
+
+//    public int getPrice(){
+//
+//        return this.price;
+//    }
 
     public void addExtraCheese(){
         // your code goes here
